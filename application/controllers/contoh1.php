7a84b4ef757f53607ae885f560b2ca92d7ef8ab7
imanuelefrata <imanuelefrata4@gmail.com>
2023-03-22 11:05:48 -0700

    menambahkan index.html,about.html,contact.html

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller 
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Imanuel Efrata Kristina 
+            Saya tinggal di daerah Pekapuran 
+             Olah raga yang saya sukai adalah Badminton";
+	}
+}
